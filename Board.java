cf3883c95262945b1da06f945accc853bc3c9ab9
Riwaz Poudyal <rp7@williams.edu>
2017-02-17 12:43:08 +0100

    adding in more code for board

--- Board.java
+++ Board.java
@@ -1,15 +1,54 @@
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Riwaz on 2/16/17.
  */
 public class Board implements BoardInterface {
 
+    Piece[][] boardArr = new Piece[Params.MAX_ROW][Params.MAX_COL];
+    List<Piece> redPieces = new ArrayList<>();
+    List<Piece> blackPieces = new ArrayList<>();
+    boolean redsTurn;
+
+    public Board(Board board) {
+        for (Piece piece : board.getRedPieces()) {
+            Piece newPiece = new Piece(this, piece);
+            redPieces.add(newPiece);
+            boardArr[piece.getCoor().getX()][piece.getCoor().getY()] = newPiece;
+        }
+
+        for (Piece piece : board.getBlackPieces()) {
+            Piece newPiece = new Piece(this, piece);
+            blackPieces.add(newPiece);
+            boardArr[piece.getCoor().getX()][piece.getCoor().getY()] = newPiece;
+        }
+
+        this.redsTurn = board.getTurn();
+    }
+
     public ArrayList<Board> possibleMoves() {
         return null;
     }
 
+    @Deprecated
     public String[][] board() {
         return null;
+    }
+
+    public Piece[][] getBoardArr() {
+        return boardArr;
+    }
+
+    public List<Piece> getRedPieces() {
+        return this.redPieces;
+    }
+
+    public List<Piece> getBlackPieces() {
+        return this.blackPieces;
+    }
+
+    public boolean getTurn() {
+        return this.redsTurn;
     }
 }
--- Piece.java
+++ Piece.java
@@ -2,4 +2,25 @@
  * Created by Riwaz on 2/16/17.
  */
 public class Piece implements PieceInterface {
+
+    protected Board board;
+    protected Coor coor;
+
+    public Piece(Board board, Coor coor) {
+        this.board = board;
+        this.coor = coor;
+    }
+
+    public Piece(Board board, Piece piece) {
+        this.board = board;
+        this.coor = piece.getCoor();
+    }
+
+    public Coor getCoor() {
+        return this.coor;
+    }
+
+    public Board getBoard() {
+        return this.board;
+    }
 }
